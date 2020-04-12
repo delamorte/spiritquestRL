c1f3405ee499b6fbaa92c15aa3cc3e417e1addf5
delamorte <dellamorte@disroot.org>
2020-04-12 15:36:23 +0300

    remove unused dir, gfx moved to /gfx

--- tilesets/adam_bolt_angband16x16_fix.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="adam_bolt_angband16x16_fix" tilewidth="16" tileheight="16" tilecount="1920" columns="32">
- <image source="adam_bolt_angband16x16_fix.png" width="512" height="960"/>
-</tileset>
--- tilesets/crawl.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="crawl" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
- <image source="../../../../../mnt/storage/downloads/DungeonCrawl_ProjectUtumnoTileset.png" width="2048" height="1536"/>
-</tileset>
