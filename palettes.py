24c258ade626961c1623b5490f3916cecd5a6de1
delamorte <dellamorte@disroot.org>
2021-05-21 22:46:10 +0300

    fix palette issues

--- palettes.py
+++ palettes.py
@@ -12,16 +12,10 @@ import variables
 dirt_colors = ["#402316", "#332925", "#4f3a28", "#4f3a28", "#3d342b", "#33221a", "#30170b", "#473c2b", "#332d16",
                "#361c18"]
 
-mod_colors = ["gray",
-              "dark gray",
-              "dark gray",
-              "darker gray",
-              "darker gray",
-              "darkest gray",
-              "darkest gray"]
-
 
 def get_dngn_colors(mod=0):
+    if variables.world_tendency != 0:
+        mod = variables.world_tendency
     color = dirt_colors[randint(0, len(dirt_colors) - 1)]
     # if mod < 0:
     #     color = "#111111"
@@ -34,11 +28,13 @@ def get_dngn_colors(mod=0):
 
 
 def get_terrain_colors(mod=None):
+    if variables.world_tendency != 0:
+        mod = variables.world_tendency
     colors = [
         "darker green",
-        "darkest green",
+        "dark green",
         "darker amber",
-        "darkest amber"
+        "dark amber"
     ]
 
     if mod is None:
@@ -46,7 +42,10 @@ def get_terrain_colors(mod=None):
 
     return colors[mod]
 
+
 def get_flower_colors(mod=0):
+    if variables.world_tendency != 0:
+        mod = variables.world_tendency
     colors = []
     if mod > 0:
         colors = ["lightest orange",
@@ -93,6 +92,8 @@ def get_flower_colors(mod=0):
 
 
 def get_forest_colors(mod=0):
+    if variables.world_tendency != 0:
+        mod = variables.world_tendency
     colors = []
     if mod > 0:
         colors = ["lightest orange",
@@ -102,8 +103,8 @@ def get_forest_colors(mod=0):
                   "light amber"]
 
     if mod < 0:
-        colors = ["darkest amber",
-                  "darker gray",
+        colors = ["darker amber",
+                  "darker amber",
                   "dark gray",
                   "green",
                   "dark green",
