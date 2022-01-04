91c5489e9270883fa0797170c8ab12cc41d13efd
delamorte <dellamorte@disroot.org>
2022-01-04 18:11:56 +0200

    Fix monster/player abilities initialization

--- components/abilities.py
+++ components/abilities.py
@@ -17,62 +17,41 @@ class Abilities:
             name = self.name
         if self.owner and self.owner.player:
             initial_abilities = json_data.data.fighters[name]["player_abilities"]
-            for n in initial_abilities:
-                item = json_data.data.abilities[n]
-                skill_type = item["skill_type"]
-                if skill_type != "weapon":
-                    continue
-                name = item["name"]
-                description = item["description"]
-                damage = item["damage"] if "damage" in item.keys() else []
-                rank = 0
-                icon = item["icon"] if "icon" in item.keys() else 12
-                dps = item["dps"] if "dps" in item.keys() else []
-                effect = item["effect"] if "effect" in item.keys() else []
-                duration = item["duration"] if "duration" in item.keys() else []
-                radius = item["radius"] if "radius" in item.keys() else []
-                chance = item["chance"] if "chance" in item.keys() else [1.0]
-                needs_ai = item["needs_ai"] if "needs_ai" in item.keys() else None
-                target_self = item["target_self"] if "target_self" in item.keys() else None
-                target_other = item["target_other"] if "target_other" in item.keys() else None
-                player_only = item["player_only"] if "player_only" in item.keys() else False
+        else:
+            initial_abilities = json_data.data.fighters[name]["abilities"]
+        for n in initial_abilities:
+            item = json_data.data.abilities[n]
+            skill_type = item["skill_type"]
+            if self.owner and self.owner.player and skill_type != "weapon":
+                continue
+            name = item["name"]
+            description = item["description"]
+            damage = item["damage"] if "damage" in item.keys() else []
+            rank = 0
+            icon = item["icon"] if "icon" in item.keys() else 12
+            dps = item["dps"] if "dps" in item.keys() else []
+            effect = item["effect"] if "effect" in item.keys() else []
+            duration = item["duration"] if "duration" in item.keys() else []
+            radius = item["radius"] if "radius" in item.keys() else []
+            chance = item["chance"] if "chance" in item.keys() else [1.0]
+            needs_ai = item["needs_ai"] if "needs_ai" in item.keys() else None
+            target_self = item["target_self"] if "target_self" in item.keys() else None
+            target_other = item["target_other"] if "target_other" in item.keys() else None
+            player_only = item["player_only"] if "player_only" in item.keys() else False
 
-                a = Ability(name=name, description=description, skill_type=skill_type, damage=damage, rank=rank,
-                            icon=icon, dps=dps, effect=effect, duration=duration, radius=radius, chance=chance,
-                            needs_ai=needs_ai, target_self=target_self, target_other=target_other,
-                            player_only=player_only)
+            a = Ability(name=name, description=description, skill_type=skill_type, damage=damage, rank=rank,
+                        icon=icon, dps=dps, effect=effect, duration=duration, radius=radius, chance=chance,
+                        needs_ai=needs_ai, target_self=target_self, target_other=target_other,
+                        player_only=player_only)
 
-                self.add_item(a)
+            self.add_item(a)
+            if self.owner and self.owner.player:
                 if skill_type == "weapon" and not self.owner.player.sel_weapon:
                     self.owner.player.sel_weapon = a
                 if skill_type == "attack" and not self.owner.player.sel_attack:
                     self.owner.player.sel_attack = a
                 if skill_type == "utility" and not self.owner.player.sel_utility:
                     self.owner.player.sel_utility = a
-        else:
-            initial_abilities = json_data.data.fighters[name]["abilities"]
-
-            for n in initial_abilities:
-                item = json_data.data.abilities[n]
-                name = item["name"]
-                description = item["description"]
-                skill_type = item["skill_type"]
-                damage = item["damage"] if "damage" in item.keys() else []
-                dps = item["dps"] if "dps" in item.keys() else []
-                effect = item["effect"] if "effect" in item.keys() else []
-                duration = item["duration"] if "duration" in item.keys() else []
-                radius = item["radius"] if "radius" in item.keys() else []
-                chance = item["chance"] if "chance" in item.keys() else [1.0]
-                needs_ai = item["needs_ai"] if "needs_ai" in item.keys() else None
-                target_self = item["target_self"] if "target_self" in item.keys() else None
-                target_other = item["target_other"] if "target_other" in item.keys() else None
-                player_only = item["player_only"] if "player_only" in item.keys() else False
-
-                a = Ability(name=name, description=description, skill_type=skill_type, damage=damage, dps=dps,
-                            effect=effect, duration=duration, radius=radius, chance=chance, needs_ai=needs_ai,
-                            target_self=target_self, target_other=target_other, player_only=player_only)
-
-                self.add_item(a)
 
     def add_item(self, item):
         """Used when adding initial abilities"""
